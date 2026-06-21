f10e3dabddfdcc466630c9eeb5a0898c3ce5b507
AndyNoob <comfortableandy9082@gmail.com>
2026-06-20 19:12:42 -0700

    Add another rectangle in playground

--- playground/main.ts
+++ playground/main.ts
@@ -3,9 +3,24 @@ import {getRotation} from "../src/dom/htmlUtil";
 import type {RectState} from "../src";
 
 const el = document.getElementById('rect')!;
+const el1 = document.getElementById('rect2')!;
 let w = (window as any);
 
-w.moveMe = createMoveMe(el, {
+const controlRoot = el.parentElement!;
+const snapping = {
+  rotation: {
+    anglesDeg: [0, 180],
+    threshold: 5
+  },
+  grid: {
+    displayThreshold: 20,
+    threshold: 5,
+    verticalX: [controlRoot.offsetWidth / 2],
+    horizontalY: [controlRoot.offsetWidth / 10 * 9]
+  }
+};
+
+w.moveMe1 = createMoveMe(el, {
   initialState: {
     x: 200,
     y: 200,
@@ -16,19 +31,23 @@ w.moveMe = createMoveMe(el, {
   onChange(next: RectState) {
     localStorage.setItem('rect', JSON.stringify(next));
   },
-  snapping: {
-    rotation: {
-      anglesDeg: [0, 180],
-      threshold: 5
-    },
-    grid: {
-      displayThreshold: 20,
-      threshold: 5,
-      verticalX: [window.innerWidth / 2],
-      horizontalY: [window.innerHeight / 10 * 9]
-    }
+  snapping,
+  controlRoot
+});
+
+w.moveMe2 = createMoveMe(el1, {
+  initialState: {
+    x: 400,
+    y: 400,
+    width: 150,
+    height: 120,
+    rotation: 299
   },
-  controlRoot: el.parentElement!
+  onChange(next: RectState) {
+    localStorage.setItem('rect', JSON.stringify(next));
+  },
+  snapping,
+  controlRoot
 });
 
 
