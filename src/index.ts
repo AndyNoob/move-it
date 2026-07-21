dc52a4d48e60cb448c1bf867de0c110df8937c26
AndyNoob <comfortableandy9082@gmail.com>
2026-07-20 22:04:51 -0700

    Export anchored functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 export { createMoveMe, computeState } from "./dom/createMoveMe";
 export { findOverlap } from "./geometry/findOverlap";
-export { convertToPercent, convertToPixels, convertFromCentered, convertToCentered } from "./geometry/state"
+export {
+  convertToPercent,
+  convertToPixels,
+  convertFromCentered,
+  convertToCentered,
+  convertFromAnchored,
+  convertToAnchored
+} from "./geometry/state"
 export { renderToCss } from "./dom/htmlUtil"
 
 export type {
